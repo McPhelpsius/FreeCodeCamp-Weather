4183ec2a40954b896490d11c97121cdfdbf125c7
Nathan Phelps <nathan@codekoalas.com>
2018-01-10 23:43:09 -0600

    Changed to wunderground api

--- LocalWeather/src/js/modules/weather.js
+++ LocalWeather/src/js/modules/weather.js
@@ -1,23 +1,43 @@
+function validateJSONResponse(response) {
+  let jsonType = response.headers
+    .get('content-type')
+    .includes('application/json');
+  if (jsonType) return response.json();
+}
+
 function GetGeolocations() {
   if (navigator.geolocation.getCurrentPosition) {
     navigator.geolocation.getCurrentPosition(geo => {
+      //use google api to get zipcode
       fetch(
-        `https://fcc-weather-api.glitch.me/api/current?lon=${
-          geo.coords.longitude
-        }&lat=${geo.coords.latitude}`,
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
+          geo.coords.latitude
+        },${geo.coords.longitude}`,
         { 'content-type': 'application/json' }
       )
         .then(response => {
-          let jsonType = response.headers
-            .get('content-type')
-            .includes('application/json');
-
-          if (jsonType) return response.json();
+          return validateJSONResponse(response);
         })
         .then(data => {
-          document.getElementById('location').innerText = data.name;
-          document.getElementById('temperature').innerText = data.main.temp;
-          document.querySelector('img').src = data.weather[0].icon;
+          fetch(
+            `http://api.wunderground.com/api/12449b9ce63b889e/conditions/q/${
+              data['results'][0]['address_components'][7]['long_name']
+            }.json`,
+            { 'content-type': 'application/json' }
+          )
+            .then(response => {
+              return validateJSONResponse(response);
+            })
+            .then(data => {
+              const currentData = data.current_observation;
+              document.getElementById('location').innerText = `${
+                currentData.display_location.full
+              }`;
+              document.getElementById('temperature').innerText = `${
+                currentData.temp_f
+              }˚F/${currentData.temp_c}˚C`;
+              document.querySelector('img').src = currentData.icon_url;
+            });
         });
     });
   }
